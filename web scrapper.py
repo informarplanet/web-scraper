7203d70646953691cd75f36fa2df17c977a50e8f
dead army <pulakp18@.com>
2024-05-26 17:10:43 +0530

    update

--- web scrapper.py
+++ web scrapper.py
@@ -1,5 +1,13 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
 from bs4 import BeautifulSoup
-html=urlopen('https://www.google.com')
-bs= BeautifulSoup(html,'lxml')
-print(bs.html)
+try:
+    html=urlopen('http://www.pythonscraping.com/pages/page1.html')
+except HTTPError as error: 
+    print(error)
+except URLError as e:
+    print('The server couldnot found ')
+else:
+    bs= BeautifulSoup(html,'lxml')
+    print(bs.html)
